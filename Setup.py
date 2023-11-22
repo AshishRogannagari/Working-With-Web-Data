eea6519a49ce90437142dd3b63b5c48948e38144
Rogannagari Ashish <125127793+AshishRogannagari@users.noreply.github.com>
2023-11-22 02:57:55 -0500

    Update Setup.py

--- Setup.py
+++ Setup.py
@@ -1,1 +1,8 @@
+from setuptools import setup
 
+setup(name='src',
+      version=0.1.0,
+      description='Working with Web Data',
+      author='Ashish Rogannagari',
+      packages=['XML_parsing','API_parsing','web_Scraping','EDA'],
+      zip_safe=False)
